0d887f303d76520202bb9d3237250d16cd84f3a5
¨KasperHdL¨ <kasper@honnens.dk>
2016-10-15 12:12:29 +0200

    Discord-canary refuse to place itself in specified space

--- i3/load_layout.sh
+++ i3/load_layout.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 i3-msg "workspace 1; append_layout ~/.config/i3/workspace_brave_corebird.json"
-i3-msg "workspace 9; append_layout ~/.config/i3/workspace_discord.json"
 i3-msg "workspace 10; append_layout ~/.config/i3/workspace_geary.json"
 i3-msg "workspace 2; append_layout ~/.config/i3/workspace_empty.json"
 
 # And finally we fill the containers with the programs they had
 (brave &)
 (corebird &)
-(discord-canary &)
 (geary &)
